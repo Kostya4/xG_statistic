2b863105783d4f7cbbe6b2df92f27f2cf3117d27
DimkaKazak <58620633+DimkaKazak@users.noreply.github.com>
2021-05-24 22:06:29 +0300

    Refactor (#2)
    Co-authored-by: Dima Kazak <dkazak@solvd.com>

--- Main.py
+++ Main.py
@@ -2,7 +2,6 @@ import pandas as pd
 from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.model_selection import train_test_split
 
-
 from utils import EventUtils
 from utils import FileUtils
 from utils import MathUtils
@@ -18,6 +17,7 @@ def retrieveData():
     shots = ShotUtils.convertAllShots(shots)
 
     return pd.DataFrame(shots)
+
 
 def main():
     data = retrieveData()
@@ -38,21 +38,36 @@ def main():
 
     data = data.drop(["positionX", "positionY"], axis=1)
 
-    xTrain = data[["matchPeriod", "Left", "Right", "Head/Body"]]
+    xTrain = data[["distance", "angle", "matchPeriod", "Left", "Right", "Head/Body"]]
     yTrain = data["Goal"]
     randomState = 12
     xTrain, xValidation, yTrain, yValidation = train_test_split(xTrain, yTrain, random_state=randomState)
 
-    learning_rates = [0.0001, 0.001, 0.01, 0.1, 0.2]
-    for learning_rate in learning_rates:
-        gradientBoostingClassifier = GradientBoostingClassifier(learning_rate=learning_rate)
+    bestAccuracy = 0
+    bestLearningRate = 0.0001
+    learningRates = [0.0001, 0.001, 0.01, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]
+    for learningRate in learningRates:
+        gradientBoostingClassifier = GradientBoostingClassifier(learning_rate=learningRate)
         gradientBoostingClassifier.fit(xTrain, yTrain)
 
-        print("Learning rate: ", learning_rate)
-        print("Accuracy score (training): {0:.3f}".format(gradientBoostingClassifier.score(xTrain, yTrain)))
-        print("Accuracy score (validation): {0:.3f}".format(gradientBoostingClassifier.score(xTrain, yTrain)))
+        print("Learning rate: ", learningRate)
+
+        trainAccuracy = gradientBoostingClassifier.score(xTrain, yTrain)
+        print("Accuracy score (training): {0:.3f}".format(trainAccuracy))
+
+        testAccuracy = gradientBoostingClassifier.score(xValidation, yValidation)
+        print("Accuracy score (validation): {0:.3f}".format(testAccuracy))
+
+        if testAccuracy > bestAccuracy:
+            bestAccuracy = testAccuracy
+            bestLearningRate = learningRate
+
         predictions = gradientBoostingClassifier.predict(xValidation)
         print(predictions)
+
+    print("Best learning rate: {}".format(bestLearningRate))
+    print("Best accuracy: {0:.3f}".format(bestAccuracy))
+
 
 if __name__ == "__main__":
     main()
